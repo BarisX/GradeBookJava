8dbebdf9dbd5d32474b6d1c37f4a028aa42c5084
Elon Must <baris.senyerli@ogr.sakarya.edu.tr>
2020-07-18 14:33:47 +0300

    Add files via upload

---
+++ GradeBook.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradebook;
+
+public class GradeBook
+5 {
+6 // display a welcome message to the GradeBook user
+7 public void displayMessage()
+8 {
+9
+10 } // end method displayMessage
+11 } // end class GradeBook
---
+++ GradeBookTest.java
@@ -0,0 +1,8 @@
+public class GradeBook
+5 {
+6 // display a welcome message to the GradeBook user
+7 public void displayMessage()
+8 {
+9
+10 } // end method displayMessage
+11 } // end class GradeBook
